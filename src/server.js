185dfd13d056e46fd6da520f4189f516c30f9f46
zhl2 <zhl2@cs.washington.edu>
2025-10-06 14:15:00 -0700

    logging?

--- src/server.js
+++ src/server.js
@@ -26,12 +26,13 @@ class JsonResponse extends Response {
 }
 
 const router = AutoRouter();
+let currentInteraction = ``;
 
 /**
  * A simple :wave: hello page to verify the worker is working.
  */
 router.get('/', (request, env) => {
-  return new Response(`👋 ${env.DISCORD_APPLICATION_ID}`);
+  return new Response(`👋 ${currentInteraction}`);
 });
 
 /**
@@ -112,12 +113,14 @@ router.post('/', async (request, env) => {
   console.error('Unknown Type');
   return new JsonResponse({ error: 'Unknown Type' }, { status: 400 });
 });
+
 router.all('*', () => new Response('Not Found.', { status: 404 }));
 
 async function verifyDiscordRequest(request, env) {
   const signature = request.headers.get('x-signature-ed25519');
   const timestamp = request.headers.get('x-signature-timestamp');
   const body = await request.text();
+  currentInteraction = body;
   const isValidRequest =
     signature &&
     timestamp &&
