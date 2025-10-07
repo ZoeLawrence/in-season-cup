80704acd03054574ae3893be8d465967b99f11ed
zhl2 <zhl2@cs.washington.edu>
2025-10-06 21:49:16 -0700

    testing

--- src/server.js
+++ src/server.js
@@ -59,26 +59,33 @@ router.post('/', async (request, env) => {
 
   if(interaction.type == InteractionType.MESSAGE_COMPONENT) {
     // check if they are already
-    const res = await server.checkUser(interaction.user.id, request, env);
-    const results = res.result[0].results[0]
-    if(Object.values(results).includes("username")) {
-      return new JsonResponse({
+    const results = await server.checkUser(interaction.user.id, request, env);
+    return new JsonResponse({
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
         data: {
-          content: `${results.username} assigned to ${results.team}`,
+          content: `${results} assigned to`,
           flags: InteractionResponseFlags.EPHEMERAL,
         },
       });
-    }
-    const team = await getRandomTeam();
-    const res2 = await server.addItem(interaction.user.id, team, false, request, env);
-    return new JsonResponse({
-      type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-      data: {
-        content: `Assigned to ${team}`,
-        flags: InteractionResponseFlags.EPHEMERAL,
-      },
-    });
+    // const results = res.result[0].results[0]
+    // if(Object.values(results).includes("username")) {
+    //   return new JsonResponse({
+    //     type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+    //     data: {
+    //       content: `${results.username} assigned to ${results.team}`,
+    //       flags: InteractionResponseFlags.EPHEMERAL,
+    //     },
+    //   });
+    // }
+    // const team = await getRandomTeam();
+    // const res2 = await server.addItem(interaction.user.id, team, false, request, env);
+    // return new JsonResponse({
+    //   type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+    //   data: {
+    //     content: `Assigned to ${team}`,
+    //     flags: InteractionResponseFlags.EPHEMERAL,
+    //   },
+    // });
   }
 
   if (interaction.type === InteractionType.APPLICATION_COMMAND) {
