0e672e82162580d6a66354a1c414aa07d9309aeb
zhl2 <zhl2@cs.washington.edu>
2025-10-07 16:19:14 -0700

    check the string of the teams

--- src/server.js
+++ src/server.js
@@ -205,15 +205,15 @@ async function checkUser(username, request, env) {
 
 async function assignTeams(results, request, env) {
   const teamList = ['CAR', 'CBJ', 'NJD', 'NYI', 'NYR', 'PHI', 'PIT', 'WSH', 'BOS', 'BUF', 'DET', 'FLA', 'MTL', 'OTT', 'TBL', 'TOR', 'CHI', 'COL', 'DAL', 'MIN', 'NSH', 'STL', 'UTA', 'WPG', 'ANA', 'CGY', 'EDM', 'LAK', 'SJS', 'SEA', 'VAN', 'VGK'];
-  const assignments = [];
+  const assignments = ``; //[];
   let x = 0;
   for(var i = teamList.length-1;i>=0;i--){
     const team = teamList.splice(Math.floor(Math.random()*teamList.length), 1);
-    assignments[x] = env.ASSIGN_DB.prepare("INSERT INTO players (team, user_id, isChamp) VALUES (?, ?, false);").bind(team, results[x].username);
+    assignments += ` ${team}`; // env.ASSIGN_DB.prepare("INSERT INTO players (team, user_id, isChamp) VALUES (?, ?, false);").bind(team, results[x].username);
     x++;
   }
   // const { results } = await env.ASSIGN_DB.batch(assignments)
-  return assignments.length;
+  return assignments;
 }
 
 async function getUsers(request, env) {
