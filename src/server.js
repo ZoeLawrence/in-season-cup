b68220261378c1741a83d3b44651bda3cfa0e9f6
zhl2 <zhl2@cs.washington.edu>
2025-10-06 22:35:12 -0700

    correct format 5

--- src/server.js
+++ src/server.js
@@ -82,7 +82,7 @@ router.post('/', async (request, env) => {
     return new JsonResponse({
       type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
       data: {
-        content: `${JSON.stringify(res)} Assigned to ${team}`,
+        content: `${interaction.member.user.id} ${JSON.stringify(res)} Assigned to ${team}`,
         flags: InteractionResponseFlags.EPHEMERAL,
       },
     });
