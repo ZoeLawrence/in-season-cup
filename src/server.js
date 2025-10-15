93909deadd9fcde3117db0f94edd306888048f82
zhl2 <zhl2@cs.washington.edu>
2025-10-15 12:36:53 -0700

    okay now check team

--- src/server.js
+++ src/server.js
@@ -179,7 +179,7 @@ router.post('/', async (request, env) => {
           type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
           data: {
             flags: InteractionResponseFlags.IS_COMPONENTS_V2,
-            content: `${results[0]}`,
+            content: `${results[0].team}`,
           },
         });
       }
