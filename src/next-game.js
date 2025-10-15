8e78576137574da647e927d0f47b262097dc6925
zhl2 <zhl2@cs.washington.edu>
2025-10-15 13:31:28 -0700

    revert to old message style

--- src/next-game.js
+++ src/next-game.js
@@ -1,17 +1,13 @@
-import {
-  InteractionResponseFlags,
-  InteractionResponseType,
-} from 'discord-interactions';
-
 export async function testAssignments(env) {
     const token = env.DISCORD_TOKEN;
     const channelId = '1425222879703990332';
     const MESSAGE = {
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-            flags: InteractionResponseFlags.IS_COMPONENTS_V2,
-            content: `Testing this style of message`,
-        },
+        content: "Hello, World!",
+        tts: false,
+        embeds: [{
+            title: "Hello, Embed!",
+            description: "This is an embedded message."
+        }]
     }
 
     if (!token) {
