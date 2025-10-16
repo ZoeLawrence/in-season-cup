4d994b857363acc89cb3624fae98392ad1f8baf5
zhl2 <zhl2@cs.washington.edu>
2025-10-15 20:34:50 -0700

    forgot the env param i hate javascript

--- src/server.js
+++ src/server.js
@@ -229,7 +229,7 @@ router.post('/', async (request, env) => {
 
             const newChampIsHome = results[0].team == match_data.homeTeam.abbrev;
 
-            await server.testUpdateMatch(match_data.game_id, match_data.game_time);
+            await server.testUpdateMatch(match_data.game_id, match_data.game_time, env);
 
             let textContent = `test: ${results[0].team} ${match_data.game_id} ${match_data.game_time}`
             // if(newChampIsHome) {
