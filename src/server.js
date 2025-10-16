33d31ba295d2689e68755d8bd67a616262ca65e0
zhl2 <zhl2@cs.washington.edu>
2025-10-15 17:40:21 -0700

    testing iso string

--- src/server.js
+++ src/server.js
@@ -219,7 +219,7 @@ router.post('/', async (request, env) => {
           type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
           data: {
             flags: InteractionResponseFlags.IS_COMPONENTS_V2,
-            content: d.toUTCString()
+            content: d.toISOString()
           }
         });
       }
