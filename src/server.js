c0ec3fd8ba606160a3f0f008c425c9c282f681b1
zhl2 <zhl2@cs.washington.edu>
2025-10-15 20:15:41 -0700

    fix value for message

--- src/server.js
+++ src/server.js
@@ -244,7 +244,7 @@ router.post('/', async (request, env) => {
               type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
               data: {
                 flags: InteractionResponseFlags.IS_COMPONENTS_V2,
-                content: textContent
+                content: `${match_data}`
               }
             });
           }
