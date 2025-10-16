c97984eede50af94ed24f2db6430e9a6afef7df9
zhl2 <zhl2@cs.washington.edu>
2025-10-16 09:53:16 -0700

    send message regardless

--- src/next-game.js
+++ src/next-game.js
@@ -73,53 +73,56 @@ export async function testAssignments(env) {
             title = `game time`
             description = game_time.toLocaleDateString()
         }
+    } else {
+        title = 'No match found'
+        description = 'use /start to set up'
+    }
 
-        const MESSAGE = {
-            tts: false,
-            embeds: [{
-                title: title,
-                description: description
-            }]
-        }
-        const token = env.DISCORD_TOKEN;
-        const channelId = '1425222879703990332';
+    const MESSAGE = {
+        tts: false,
+        embeds: [{
+            title: title,
+            description: description
+        }]
+    }
+    const token = env.DISCORD_TOKEN;
+    const channelId = '1425222879703990332';
 
-        if (!token) {
-            throw new Error('The DISCORD_TOKEN environment variable is required.');
-        }
-        if (!channelId) {
-            throw new Error(
-                'The DISCORD_APPLICATION_ID environment variable is required.',
-            );
-        }
+    if (!token) {
+        throw new Error('The DISCORD_TOKEN environment variable is required.');
+    }
+    if (!channelId) {
+        throw new Error(
+            'The DISCORD_APPLICATION_ID environment variable is required.',
+        );
+    }
 
-        const url = `https://discord.com/api/v10/channels/${channelId}/messages`;
+    const url = `https://discord.com/api/v10/channels/${channelId}/messages`;
 
-        const response = await fetch(url, {
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bot ${token}`,
-            },
-            method: 'POST',
-            body: JSON.stringify(MESSAGE),
-        });
+    const response = await fetch(url, {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bot ${token}`,
+        },
+        method: 'POST',
+        body: JSON.stringify(MESSAGE),
+    });
 
-        if (response.ok) {
-            console.log('Registered all commands');
-            const data = await response.json();
-            console.log(JSON.stringify(data, null, 2));
-        } else {
-            console.error('Error registering commands');
-            let errorText = `Error registering commands \n ${response.url}: ${response.status} ${response.statusText}`;
-            try {
-                const error = await response.text();
-                if (error) {
-                errorText = `${errorText} \n\n ${error}`;
-                }
-            } catch (err) {
-                console.error('Error reading body from request:', err);
+    if (response.ok) {
+        console.log('Registered all commands');
+        const data = await response.json();
+        console.log(JSON.stringify(data, null, 2));
+    } else {
+        console.error('Error registering commands');
+        let errorText = `Error registering commands \n ${response.url}: ${response.status} ${response.statusText}`;
+        try {
+            const error = await response.text();
+            if (error) {
+            errorText = `${errorText} \n\n ${error}`;
             }
-            console.error(errorText);
+        } catch (err) {
+            console.error('Error reading body from request:', err);
         }
+        console.error(errorText);
     }
 }
