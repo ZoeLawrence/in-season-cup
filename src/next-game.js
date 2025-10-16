386e84093c5fd5675db2afac7562a47d6680a326
zhl2 <zhl2@cs.washington.edu>
2025-10-16 10:32:20 -0700

    re-testing again

--- src/next-game.js
+++ src/next-game.js
@@ -8,51 +8,53 @@ export async function testAssignments(env) {
 		const game_time = new Date(currentMatch[0].datetime);
 		const current_time = new Date();
 		if (current_time.getTime() > game_time.getTime()) {
-			const game_data = await getNHLData(`gamecenter/${currentMatch[0].game_id}/landing`);
-			const away_abbr = game_data.awayTeam.abbrev;
-			const home_abbr = game_data.homeTeam.abbrev;
+			title = `current time`
+			description = current_time.toLocaleDateString()
+			// const game_data = await getNHLData(`gamecenter/${currentMatch[0].game_id}/landing`);
+			// const away_abbr = game_data.awayTeam.abbrev;
+			// const home_abbr = game_data.homeTeam.abbrev;
 
-			const currentChampIsHome = currentMatch[0].team == home_abbr;
-			const winnerIsHome = game_data.homeTeam.score > game_data.awayTeam.score;
+			// const currentChampIsHome = currentMatch[0].team == home_abbr;
+			// const winnerIsHome = game_data.homeTeam.score > game_data.awayTeam.score;
 
-			const stmt = env.ASSIGN_DB.prepare("UPDATE players SET isChamp = ? WHERE team = ?;")
-			if (currentChampIsHome != winnerIsHome) {
-				await env.ASSIGN_DB.batch([
-					stmt.bind(!winnerIsHome, away_abbr),
-					stmt.bind(winnerIsHome, home_abbr)
-				]);
-				title = `wins the cup!`;
-			} else {
-				title = `retains the cup!`
-			}
+			// const stmt = env.ASSIGN_DB.prepare("UPDATE players SET isChamp = ? WHERE team = ?;")
+			// if (currentChampIsHome != winnerIsHome) {
+			// 	await env.ASSIGN_DB.batch([
+			// 		stmt.bind(!winnerIsHome, away_abbr),
+			// 		stmt.bind(winnerIsHome, home_abbr)
+			// 	]);
+			// 	title = `wins the cup!`;
+			// } else {
+			// 	title = `retains the cup!`
+			// }
 
-			// const results = await server.getChamp(env);
-			const newChamp = await getNewChamp()
+			// // const results = await server.getChamp(env);
+			// const newChamp = await getNewChamp()
 
-			// const game_data = await getCurrentMatchup(results[0].team, env);
-			const match_data = await getCurrentMatchup(newChamp[0].team, env);
+			// // const game_data = await getCurrentMatchup(results[0].team, env);
+			// const match_data = await getCurrentMatchup(newChamp[0].team, env);
 
-			const awayTeam = `${match_data.awayTeam.placeName.default} ${match_data.awayTeam.commonName.default}`;
-			const homeTeam = `${match_data.homeTeam.placeName.default} ${match_data.homeTeam.commonName.default}`;
+			// const awayTeam = `${match_data.awayTeam.placeName.default} ${match_data.awayTeam.commonName.default}`;
+			// const homeTeam = `${match_data.homeTeam.placeName.default} ${match_data.homeTeam.commonName.default}`;
 
-			// const winnerIsHome = results[0].team == game_data.homeTeam.abbrev;
-			const newChampIsHome = newChamp[0].team == match_data.homeTeam.abbrev;
+			// // const winnerIsHome = results[0].team == game_data.homeTeam.abbrev;
+			// const newChampIsHome = newChamp[0].team == match_data.homeTeam.abbrev;
 
-			// await server.createFirstMatch(game_data.game_id, game_data.game_time, env);
-			await getNextMatch(match_data.game_id, match_data.game_time, env);
+			// // await server.createFirstMatch(game_data.game_id, game_data.game_time, env);
+			// await getNextMatch(match_data.game_id, match_data.game_time, env);
 
-			const game_day = new Date(match_data.game_time);
+			// const game_day = new Date(match_data.game_time);
 
-			const player = env.ASSIGN_DB.prepare("SELECT * FROM players WHERE team = ?;")
-			const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-			if (newChampIsHome) {
-				const { away } = await player.bind(match_data.awayTeam.abbrev).run();
-				description += `<@${newChamp[0].user_id}>'s ${homeTeam} will move on to face <@${away[0].user_id}>'s ${awayTeam} on ${days[game_day.getDay()]}!`;
-			} else {
-				const { home } = await player.bind(match_data.homeTeam.abbrev).run();
-				description += `<@${newChamp[0].user_id}>'s ${awayTeam} will move on to face <@${home[0].user_id}>'s ${homeTeam} on ${days[game_day.getDay()]}!`;
-			}
-			title = `# <@${newChamp[0].user_id}> ${title}`;
+			// const player = env.ASSIGN_DB.prepare("SELECT * FROM players WHERE team = ?;")
+			// const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+			// if (newChampIsHome) {
+			// 	const { away } = await player.bind(match_data.awayTeam.abbrev).run();
+			// 	description += `<@${newChamp[0].user_id}>'s ${homeTeam} will move on to face <@${away[0].user_id}>'s ${awayTeam} on ${days[game_day.getDay()]}!`;
+			// } else {
+			// 	const { home } = await player.bind(match_data.homeTeam.abbrev).run();
+			// 	description += `<@${newChamp[0].user_id}>'s ${awayTeam} will move on to face <@${home[0].user_id}>'s ${homeTeam} on ${days[game_day.getDay()]}!`;
+			// }
+			// title = `# <@${newChamp[0].user_id}> ${title}`;
 		} else {
 			title = `game time`
 			description = game_time.toLocaleDateString()
