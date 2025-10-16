4eda987e4d9defac31006d013ca1cdd26d59855a
zhl2 <zhl2@cs.washington.edu>
2025-10-15 20:14:51 -0700

    remove some code to test where failure is

--- src/server.js
+++ src/server.js
@@ -224,21 +224,21 @@ router.post('/', async (request, env) => {
             const results = await server.getChamp(env);
             const match_data = await getCurrentMatchup(results[0].team, env);
 
-            const awayTeam = game_data.awayTeam.commonName.default;
-            const homeTeam = game_data.homeTeam.commonName.default;
+            // const awayTeam = game_data.awayTeam.commonName.default;
+            // const homeTeam = game_data.homeTeam.commonName.default;
 
-            const newChampIsHome = results[0].team == match_data.homeTeam.abbrev;
+            // const newChampIsHome = results[0].team == match_data.homeTeam.abbrev;
 
-            await testUpdateMatch(match_data.game_id, match_data.game_time);
+            // await testUpdateMatch(match_data.game_id, match_data.game_time);
 
-            let textContent = ``
-            if(newChampIsHome) {
-                const away = await server.getUser(match_data.awayTeam.abbrev, env);
-                textContent +=  `Next match up: <@${away[0].user_id}>'s ${awayTeam} faces <@${results[0].user_id}>'s ${homeTeam}`;
-            } else {
-                const home = await server.getUser(match_data.homeTeam.abbrev, env);
-                textContent += `Next match up: <@${home[0].user_id}>'s ${homeTeam} faces <@${results[0].user_id}>'s ${awayTeam}`;
-            }
+            // let textContent = ``
+            // if(newChampIsHome) {
+            //     const away = await server.getUser(match_data.awayTeam.abbrev, env);
+            //     textContent +=  `Next match up: <@${away[0].user_id}>'s ${awayTeam} faces <@${results[0].user_id}>'s ${homeTeam}`;
+            // } else {
+            //     const home = await server.getUser(match_data.homeTeam.abbrev, env);
+            //     textContent += `Next match up: <@${home[0].user_id}>'s ${homeTeam} faces <@${results[0].user_id}>'s ${awayTeam}`;
+            // }
 
             return new JsonResponse({
               type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
