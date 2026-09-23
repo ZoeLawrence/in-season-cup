642dd62a2c8463a3316d245c284a58826257fa4a
zhl2 <zhl2@cs.washington.edu>
2026-09-22 18:03:39 -0700

    even more testing

--- src/server.js
+++ src/server.js
@@ -241,7 +241,7 @@ router.post('/', async (request, env) => {
           type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
           data: {
             flags: InteractionResponseFlags.IS_COMPONENTS_V2,
-            content: `${channel} ${d.toISOString()}`,
+            content: `${channel[0]} ${d.toISOString()}`,
           },
         });
       }
